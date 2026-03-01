61898978cdf29e3fa57e66b0fac44bfbfbbe3748
Eric Mika <kitschpatrol@users.noreply.github.com>
2026-03-01 05:15:24 +0000

    test-formula-git-revision 0.8.2

--- Formula/test-formula-git-revision.rb
+++ Formula/test-formula-git-revision.rb
@@ -2,8 +2,8 @@ class TestFormulaGitRevision < Formula
   desc "Formula to test Action"
   homepage "https://github.com/Debian/dh-make-golang"
   url "https://github.com/Debian/dh-make-golang.git",
-      tag: "v0.3.2",
-      revision: "82916c0d56b6319398f635199222dff397fafc12"
+      tag: "v0.8.2",
+      revision: "6eec7672e012a819bb9cd6b234f379de9dc80304"
   license "MIT"
   head "https://github.com/Debian/dh-make-golang.git"
 
