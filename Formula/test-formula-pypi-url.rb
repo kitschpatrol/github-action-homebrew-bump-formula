5c7027851317eda1044b6230dd83c28d7f6a1033
Eric Mika <eric@ericmika.com>
2026-02-28 22:08:56 -0500

    Fix workflow.

--- Formula/test-formula-pypi-url.rb
+++ Formula/test-formula-pypi-url.rb
@@ -8,7 +8,7 @@ class TestFormulaPypiUrl < Formula
   license "MIT"
   head "https://github.com/commitizen-tools/commitizen.git"
 
-  depends_on "python@3.9"
+  depends_on "python@3.13"
 
   resource "argcomplete" do
     url "https://files.pythonhosted.org/packages/cb/53/d2e3d11726367351b00c8f078a96dacb7f57aef2aca0d3b6c437afc56b55/argcomplete-1.12.2.tar.gz"
